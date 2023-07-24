1a9262444bbf5a5c4ea7635228f910e68ef8fa93
Herman Barnardt <herman.barnardt@standardbank.co.za>
2023-07-24 17:07:27 +0200

    feat: Add io methods as framework

---
+++ java-forum-concurrent-io/src/main/za/co/entelect/java/forum/io/DadJokeClient.java
@@ -0,0 +1,30 @@
+package za.co.entelect.java.forum.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class DadJokeClient {
+
+    final HttpClient client;
+
+    public DadJokeClient() {
+        client = HttpClient.newHttpClient();
+    }
+
+    public HttpResponse<InputStream> getResponse(String jokeId) throws IOException, InterruptedException {
+        return client.send(createRequest(jokeId), HttpResponse.BodyHandlers.ofInputStream());
+    }
+
+    public HttpRequest createRequest(String jokeId) {
+        return HttpRequest.newBuilder().build();
+    }
+
+    private URI creatURI(String jokeId) {
+        return URI.create("https://icanhazdadjoke.com/j/" + jokeId + ".png");
+    }
+
+}
---
+++ java-forum-concurrent-io/src/main/za/co/entelect/java/forum/io/FileUtils.java
@@ -0,0 +1,15 @@
+package za.co.entelect.java.forum.io;
+
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
+
+public class FileUtils {
+
+    public static List<String> readAllLines(String path) {
+        return Collections.emptyList();
+    }
+
+    public static void saveToFile(InputStream input, String fileName) {
+    }
+}
