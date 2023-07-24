a05695baaa3dcd2ceb7311615a2298ce66df6e24
Herman Barnardt <herman.barnardt@standardbank.co.za>
2023-07-24 17:38:45 +0200

    feat: Implement IO functions

--- src/main/za/co/entelect/java/forum/io/FileUtils.java
+++ src/main/za/co/entelect/java/forum/io/FileUtils.java
@@ -1,15 +1,28 @@
 package za.co.entelect.java.forum.io;
 
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collections;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 public class FileUtils {
 
     public static List<String> readAllLines(String path) {
-        return Collections.emptyList();
+        Path file = Path.of(path);
+        try {
+            return Files.readAllLines(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static void saveToFile(InputStream input, String fileName) {
+        Path file = Path.of(fileName);
+        try {
+            Files.copy(input, file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
